1174306b578372800d9012e2100cd6b85397029a
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-05-13 23:24:16 +0200

    Added reactive log in form

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../shared/services/auth.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,29 @@ import { AuthService } from '../shared/services/auth.service';
 export class LoginPage implements OnInit {
   constructor(private router: Router, private auth: AuthService) {}
 
-  ngOnInit() {}
+  form: FormGroup;
+
+  generateForm() {
+    this.form = new FormGroup({
+      user: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.minLength(8), Validators.required],
+      }),
+
+      pass: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.minLength(8), Validators.required],
+      }),
+    });
+  }
+
+  ionViewWillEnter() {
+    this.generateForm();
+  }
+
+  ngOnInit() {
+    this.generateForm();
+  }
 
   onLogin() {
     this.auth.logIn();
