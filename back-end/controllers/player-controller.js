d9c15552a1144afd03b5432c19527f731ff75670
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-05-16 22:26:46 +0200

    Added post player route

--- back-end/controllers/player-controller.js
+++ back-end/controllers/player-controller.js
@@ -9,4 +9,17 @@ exports.GetAllPlayers = async (req, res, next) => {
   }
 };
 
-exports.PostPlayer;
+exports.PostPlayer = async (req, res, next) => {
+  const player = new Player({
+    name: req.body.name,
+    jerseyNumber: req.body.jerseyNumber,
+    imageUrl: req.body.imageUrl,
+  });
+
+  try {
+    const response = await player.save();
+    res.status(201).json({ signal: true });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
