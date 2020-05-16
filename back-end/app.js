04bf475eb295ffd3beccaee9af0268981911f664
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-05-16 20:46:26 +0200

    Added server.js and app.js

---
+++ back-end/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res, next) {
+  res.send('Server started');
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, PUT, OPTIONS'
+  );
+  next();
+});
+
+module.exports = app;
