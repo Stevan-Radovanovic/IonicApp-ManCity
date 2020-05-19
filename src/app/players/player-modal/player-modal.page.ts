9b35dc8b1f7fbdbcf4c9a9ce691e34be095598d4
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-05-19 15:57:38 +0200

    Added new player reactive form

--- src/app/players/player-modal/player-modal.page.ts
+++ src/app/players/player-modal/player-modal.page.ts
@@ -1,18 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { environment } from 'src/environments/environment';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-player-modal',
   templateUrl: './player-modal.page.html',
   styleUrls: ['./player-modal.page.scss'],
 })
 export class PlayerModalPage implements OnInit {
+  form: FormGroup;
+
   constructor(private modalCtrl: ModalController) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.generateForm();
+  }
 
   onDismissModal() {
     this.modalCtrl.dismiss();
     console.log('%c ALERT: Modal Dissmised', environment.consoleLog);
+  }
+
+  generateForm() {
+    this.form = new FormGroup({
+      name: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.required],
+      }),
+
+      imageUrl: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.required],
+      }),
+      jerseyNumber: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.required],
+      }),
+    });
+  }
+
+  ionViewWillEnter() {
+    this.generateForm();
   }
 }
