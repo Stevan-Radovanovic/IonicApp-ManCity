c3d0cfb1844a224512d40f5f1530bdca94528d89
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-05-22 14:17:26 +0200

    Added login and register backend functions v1.0

--- back-end/controllers/user-controller.js
+++ back-end/controllers/user-controller.js
@@ -2,6 +2,53 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user-model');
 
-exports.Register = (req, res, next) => {};
+exports.Register = async (req, res, next) => {
+  try {
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const user = new User({
+      email: req.body.email,
+      username: req.body.username,
+      password: hash,
+    });
+    const result = await user.save();
+    res.status(201).json({
+      signal: true,
+    });
+  } catch (err) {
+    res.status(500).json({ error: 'Sign Up Failed' });
+  }
+};
 
-exports.LogIn = (req, res, next) => {};
+exports.LogIn = async (req, res, next) => {
+  try {
+    const fetchedUser = await User.findOne({ email: req.body.email });
+    if (!fetchedUser) {
+      return res.status(401).json({
+        message: 'Log In failed',
+      });
+    }
+    const compareResult = await bcrypt.compare(
+      req.body.password,
+      fetchedUser.password
+    );
+    if (!compareResult) {
+      return res.status(401).json({
+        message: 'Log In failed',
+      });
+    }
+    const token = jwt.sign(
+      { email: fetchedUser.email, id: fetchedUser._id },
+      'secret',
+      {
+        expiresIn: '1h',
+      }
+    );
+    res.status(200).json({
+      token: token,
+    });
+  } catch (err) {
+    res.status(401).json({
+      message: 'Log In failed',
+    });
+  }
+};
